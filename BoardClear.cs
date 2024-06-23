010ee8b92c4bc10cba963c79b9b7a1b0194887a1
Anna Blood <ab00721@my.westga.edu>
2024-06-23 17:08:32 -0400

    Test Board methods clear and count

---
+++ BoardClear.cs
@@ -0,0 +1,67 @@
+﻿using WestSetGame.Models;
+
+namespace WestSetGame.Tests
+{
+    [TestClass]
+    public class BoardClear
+    {
+        private static readonly int BOARD_SIZE = 12;
+
+        [TestMethod]
+        public void TestShouldEmptyAFullBoard()
+        {
+            Deck deck = new Deck();
+            Board board = new Board();
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                board.AddCard(deck.DrawCard()!, i);
+            }
+            board.Clear();
+            foreach (var card in board.GetCards())
+            {
+                Assert.IsNull(card, "All spots on the board should be null after clearing.");
+            }
+        }
+
+        [TestMethod]
+        public void TestShouldEmptyAPartiallyFullBoard()
+        {
+            Deck deck = new Deck();
+            Board board = new Board();
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                board.AddCard(deck.DrawCard()!, i);
+            }
+            board.RemoveCard(0);
+            board.RemoveCard(1);
+            board.RemoveCard(2);
+            board.Clear();
+            foreach (var card in board.GetCards())
+            {
+                Assert.IsNull(card, "All spots on the board should be null after clearing.");
+            }
+        }
+
+        [TestMethod]
+        public void TestShouldResetTheBoardCount()
+        {
+            Deck deck = new Deck();
+            Board board = new Board();
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                board.AddCard(deck.DrawCard()!, i);
+            }
+            board.Clear();
+            Assert.AreEqual(0, board.Count(), "There should be zero non-null cards after clear.");
+        }
+
+        [TestMethod]
+        public void TestShouldClearAlreadyEmptyBoard()
+        {
+            Deck deck = new Deck();
+            Board board = new Board();
+            board.Clear();
+            Assert.AreEqual(0, board.Count(), "There should be zero non-null cards after 'clear' empty board.");
+        }
+    }
+}
---
+++ BoardCount.cs
@@ -0,0 +1,75 @@
+﻿using WestSetGame.Models;
+
+namespace WestSetGame.Tests
+{
+    [TestClass]
+    public class BoardCount
+    {
+        private static readonly int BOARD_SIZE = 12;
+
+        [TestMethod]
+        public void TestShouldReturnZeroForEmptyBoard()
+        {
+            Deck deck = new Deck();
+            Board board = new Board();
+            Assert.AreEqual(0, board.Count(), "Empty board should have zero cards.");
+        }
+
+        [TestMethod]
+        public void TestShouldReturnBoardSizeForFullBoard()
+        {
+            Deck deck = new Deck();
+            Board board = new Board();
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                board.AddCard(deck.DrawCard()!, i);
+            }
+            Assert.AreEqual(BOARD_SIZE, board.Count(), "Full board should have board size of cards.");
+
+        }
+
+        [TestMethod]
+        public void TestShouldReturnCorrectCountForPartiallyFilledBoard()
+        {
+            Deck deck = new Deck();
+            Board board = new Board();
+            for (int i = 0; i < BOARD_SIZE/2; i++)
+            {
+                board.AddCard(deck.DrawCard()!, i);
+            }
+            Assert.AreEqual(BOARD_SIZE/2, board.Count(), "Full board should have board size of cards.");
+
+        }
+
+        [TestMethod]
+        public void TestShouldReturnCorrectCountAfterRemovingCards()
+        {
+            Deck deck = new Deck();
+            Board board = new Board();
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                board.AddCard(deck.DrawCard()!, i);
+            }
+            board.RemoveCard(3);
+            board.RemoveCard(4);
+            board.RemoveCard(5);
+
+            Assert.AreEqual(BOARD_SIZE - 3, board.Count(), "Full board should have board size of cards.");
+
+        }
+
+        [TestMethod]
+        public void TestShouldReturnZeroAfterClearingBoard()
+        {
+            Deck deck = new Deck();
+            Board board = new Board();
+            for (int i = 0; i < BOARD_SIZE / 2; i++)
+            {
+                board.AddCard(deck.DrawCard()!, i);
+            }
+            board.Clear();
+            Assert.AreEqual(0, board.Count(), "Cleared board should have 0 cards.");
+
+        }
+    }
+}
