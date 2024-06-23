699ad2b1f05cc3b414123f45463a6d3193762f17
Anna Blood <ab00721@my.westga.edu>
2024-06-23 10:23:14 -0400

    Add Deck validtion and tests

--- DeckDrawCard.cs
+++ DeckDrawCard.cs
@@ -8,9 +8,60 @@ namespace WestSetGame.Tests
         private static readonly int FULL_DECK = 81;
 
         [TestMethod]
-        public void Test()
+        public void TestShouldReturnCardWhenDeckIsFull()
         {
-
+            Deck deck = new Deck();
+            Card? card = deck.DrawCard();
+            Assert.IsNotNull(card, "Card drawn from full deck should not be null.");
         }
+
+        [TestMethod]
+        public void TestShouldReturnCardWhenDeckIsNotFull()
+        {
+            Deck deck = new Deck();
+            for (int i = 0; i < 10; i++)
+            {
+                deck.DrawCard();
+            }
+            Card? card = deck.DrawCard();
+            Assert.IsNotNull(card, "Card drawn from parial deck is not null.");
+        }
+
+        [TestMethod]
+        public void TestShouldReturnNullWhenDeckIsEmpty()
+        {
+            Deck deck = new Deck();
+            for (int i = 0; i < FULL_DECK; i++)
+            {
+                deck.DrawCard();
+            }
+            Card? card = deck.DrawCard();
+            Assert.IsNull(card, "Card drawn from empty deck is null.");
+        }
+
+        [TestMethod]
+        public void TestShouldDecreaseDeckCount()
+        {
+            Deck deck = new Deck();
+            int initDeckSize = deck.Count();
+            deck.DrawCard();
+            int newDeckSize = deck.Count();
+            Assert.IsTrue(newDeckSize < initDeckSize);
+        }
+
+        [TestMethod]
+        public void TestShuoldReturnDifferentCardsInEachDraw()
+        {
+            Deck deck = new Deck();
+            List<Card> cards = new List<Card>();
+            for (int i = 0; i < FULL_DECK; i++)
+            {
+                Card? nextCard = deck.DrawCard(); ;
+                Assert.IsNotNull(nextCard, "Next card should not be null.");
+                Assert.IsFalse(cards.Contains(nextCard));
+                cards.Add(nextCard);
+            }
+        }
+
     }
 }
--- DeckReturnToDeck.cs
+++ DeckReturnToDeck.cs
@@ -1,4 +1,5 @@
 ﻿using WestSetGame.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WestSetGame.Tests
 {
@@ -8,9 +9,57 @@ namespace WestSetGame.Tests
         private static readonly int FULL_DECK = 81;
 
         [TestMethod]
-        public void Test()
+        public void TestShouldAddCardBackToDeck()
         {
+            Deck deck = new Deck();
+            Card card = deck.DrawCard()!;
+            Assert.IsFalse(deck.GetDeck().Contains(card), "Card is not in deck after it is drawn.");
+            deck.ReturnToDeck(card);
+            Assert.IsTrue(deck.GetDeck().Contains(card), "Card is back in deck after it is returned.");
+        }
+        
+        [TestMethod]
+        public void TestShouldThrowErrorIfCardReturnedIsAlreadyInDeck()
+        {
+            Deck deck = new Deck();
+            Card card = deck.GetDeck()[0];
+            Assert.IsTrue(deck.GetDeck().Contains(card), "Card is is in deck.");
+            try
+            {
+                deck.ReturnToDeck(card);
+                Assert.Fail("An exception should have been thrown");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual("Card is already in deck", ex.Message);
+            }
+        }
 
+        [TestMethod]
+        public void TestShouldThrowErrorIfCardReturnedIsNull()
+        {
+            Deck deck = new Deck();
+            try
+            {
+                deck.ReturnToDeck(null);
+                Assert.Fail("An exception should have been thrown");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.AreEqual("Value cannot be null. (Parameter 'card')", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestShouldIncreaseNumberOfCardsInDeck()
+        {
+            Deck deck = new Deck();
+            Card card = deck.DrawCard()!;
+            int drawn = deck.Count();
+            deck.ReturnToDeck(card);
+            int returned = deck.Count();
+            Assert.IsTrue(returned > drawn, "Deck count increases after card is returned");
+            Assert.AreEqual(drawn + 1, returned, "Deck count is incremented by 1 when returned.");
         }
     }
 }
